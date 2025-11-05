67c981a221e3bdebf7018e5e10ab51da1d4f51d0
myappstorefreenf-source <myappstore.free.nf@gmail.com>
2025-11-05 19:28:06 -0300

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -560,7 +560,7 @@ function MasVideosGrid({ categoria, videos, onPlay, onClose }) {
                     </button>
                 </div>
 
-                <div className="grid grid-cols-2 sm:grid-cols-8 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                <div className="grid grid-cols-2 sm:grid-cols-7 lg:grid-cols-7 xl:grid-cols-7 gap-4">
                     {videos.map((video, index) => (
                         <ReproductorDeVideo 
                             key={index} 
@@ -825,7 +825,7 @@ function App() {
                         <VideoCarousel>
                             
                             {videosEnCarrusel.map((video, index) => (
-                                <div key={index} className="flex-shrink-0 w-40 sm:w-52 lg:w-64">
+                                <div key={index} className="flex-shrink-0 w-20 sm:w-52 lg:w-64">
                                     <ReproductorDeVideo 
                                         titulo={video.titulo} 
                                         url={video.url} 
@@ -857,6 +857,7 @@ function App() {
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
+
 
 
 

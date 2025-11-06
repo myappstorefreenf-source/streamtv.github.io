9bb8677d45e8cdb2339597816639530f48f2919a
myappstorefreenf-source <myappstore.free.nf@gmail.com>
2025-11-06 17:44:38 -0300

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -540,12 +540,12 @@ function MasVideosGrid({ categoria, videos, onPlay, onClose }) {
         };
     }, [handleGridDpadNavigation]);
 
-
+//sticky sacado para fijar barra delante del  top
     return (
         <div ref={gridRef} className="mas-videos-grid fixed inset-0 bg-gray-900/95 z-40 overflow-y-auto p-4 md:p-8" tabIndex={0} style={{ outline: 'none' }}>
             <div className="max-w-7xl mx-auto">
                 
-                <div className="flex justify-between items-center mb-6 sticky top-0 bg-gray-900/90 py-2 z-10">
+                <div className="flex justify-between items-center mb-6  top-0 bg-gray-900/90 py-2 z-10">
                     <h1 className="text-3xl font-bold text-withe-600 capitalize">
                          {categoria}
                     </h1>
@@ -857,3 +857,4 @@ function App() {
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
+

8642fda9f2942762904b1fedc8339a8b034ebda7
myappstorefreenf-source <myappstore.free.nf@gmail.com>
2025-11-24 18:45:40 -0300

    Actualizar tv.jsx

--- tv.jsx
+++ tv.jsx
@@ -545,7 +545,7 @@ const VideoPlayer = React.forwardRef(({ channel, isPlaying, onFinish }, ref) => 
             const hlsConfig = {
                 // Configuración de HLS para búfer y headers
                 maxBufferLength: 30,     
-                minBufferLength: 3,      
+                minBufferLength: 10,      
                 autoSyncBuffer: 0.5,
                 xhrSetup: setupXhr 
             };

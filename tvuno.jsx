389a241e090b7075f1e5a3c09ba570e37394e2ba
myappstorefreenf-source <myappstore.free.nf@gmail.com>
2025-12-18 11:08:25 -0300

    Update tvuno.jsx

--- tvuno.jsx
+++ tvuno.jsx
@@ -40,6 +40,7 @@ const LOCAL_M3U_DATA = [
     logoUrl: "https://images.seeklogo.com/logo-png/45/1/telefe-tv-logo-png_seeklogo-451860.png",
     category: "Argentina",
     workerId: 'telefe', //identifica el worker para ser llamado y generar el token
+     url: "https://telefe.com/Api/Videos/GetSourceUrl/694564/0/HLS?.m3u8"
 },
     {
         title: "Canal 26",
@@ -2057,3 +2058,4 @@ if (rootElement) {
 } else {
     console.error("No se encontró el elemento 'root'. Asegúrate de que tu HTML tiene <div id='root'></div>");
 }
+

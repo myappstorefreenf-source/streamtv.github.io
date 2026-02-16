4caa92b662e9bfbca94da29e521c8c8d0faeb3dc
myappstorefreenf-source <myappstore.free.nf@gmail.com>
2026-02-16 15:38:53 -0300

    Actualizar tvuno.jsx

--- tvuno.jsx
+++ tvuno.jsx
@@ -14,9 +14,7 @@ const LOCAL_M3U_DATA = [
         title: "Prueba TV",
         logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Logotipo_de_America_TV.svg/1933px-Logotipo_de_America_TV.svg.png",
         category: "Argentina",
-        url: "https://mplus.pontiscloud.com/live/eds/A24/live_dash_cld/A24.mpd?|referer=https://webclient.antinaplayplus.com.ar/
-
-",
+        url: "https://mplus.pontiscloud.com/live/eds/A24/live_dash_cld/A24.mpd?|referer=https://webclient.antinaplayplus.com.ar/",
          referrer: "https://cdn1.devilsfilm.org/", 
         userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36" 
     },

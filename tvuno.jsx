741e68ef325893ea67f7ca975b67f948e07ec631
myappstorefreenf-source <myappstore.free.nf@gmail.com>
2026-02-24 23:31:40 -0300

    Update tvuno.jsx

--- tvuno.jsx
+++ tvuno.jsx
@@ -15,6 +15,8 @@ const LOCAL_M3U_DATA = [
         logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Logotipo_de_America_TV.svg/1933px-Logotipo_de_America_TV.svg.png",
         category: "Argentina",
         url: "http://104.238.205.58:9090/283887_.m3u8",
+           referrer: "http://104.238.205.58:9090/283887_.m3u8/",
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
         
        // userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36" 
     },
@@ -2109,6 +2111,7 @@ if (rootElement) {
 } else {
     console.error("No se encontró el elemento 'root'. Asegúrate de que tu HTML tiene <div id='root'></div>");
 }
+
 
 
 

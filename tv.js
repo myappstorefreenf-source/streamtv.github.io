6f2937841c41d18af6fe5f6d502d648650fabbcf
myappstorefreenf-source <myappstore.free.nf@gmail.com>
2026-05-03 22:08:49 -0300

    Crear tv.js

---
+++ tv.js
@@ -0,0 +1,7 @@
+window.m3uTV =`
+#EXTM3U
+#EXTINF:-1 tvg-id="" tvg-name="El demonio vestido de azul (1995)" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/fxzA7U1eQK7fr5z7rF6df4vn1ZR.jpg" group-title="▶️ SUSPENSO ▶️",El demonio vestido de azul (1995) 
+http://campstvpro.com:8080/movie/CNT397pe/5322GT2Uax3r/597410.mp4 
+#EXTINF:-1 tvg-id="" tvg-name="Aún es de Noche en Caracas (2025)" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/j3lFMpDBH6GaZqQD9bZgSY2yoqQ.jpg" group-title="▶️ 2025▶️",Aún es de Noche en Caracas (2025) 
+http://campstvpro.com:8080/movie/CNT397pe/5322GT2Uax3r/597406.mp4 
+#EXTINF:-1 tvg-id="" tvg-name="La virgen de la tosquera (2026)" tvg-logo="https://image.tmdb.org/t/p/w600_and_h900_bestv2/qoejV5MRn2bl6kPJBAC3OC1qPMT.jpg" group-title="▶️ ESTRENOS 2026 ▶️",La virgen de la tosquera (2026)`
